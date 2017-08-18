4f4ff758bb44ba3231b90c1a479e8a0d11bfedb7
Aviv Bachan <avivbd@users.noreply.github.com>
2017-08-18 13:08:52 -0700

    modified sklearn LabelEncoder to issue a warning rather than an error and keep track of the unkown classes

---
+++ LabelEncoder.py
@@ -0,0 +1,145 @@
+
+from collections import defaultdict
+import itertools
+import array
+
+import numpy as np
+import scipy.sparse as sp
+
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+from sklearn.utils.fixes import sparse_min_max
+from sklearn.utils import column_or_1d
+from sklearn.utils.validation import check_array
+from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.validation import _num_samples
+from sklearn.utils.multiclass import unique_labels
+from sklearn.utils.multiclass import type_of_target
+
+from sklearn.externals import six
+
+class LabelEncoder(BaseEstimator, TransformerMixin):
+    """Encode labels with value between 0 and n_classes-1.
+
+    Read more in the :ref:`User Guide <preprocessing_targets>`.
+
+    Attributes
+    ----------
+    classes_ : array of shape (n_class,)
+        Holds the label for each class.
+
+    Examples
+    --------
+    `LabelEncoder` can be used to normalize labels.
+
+    >>> from sklearn import preprocessing
+    >>> le = preprocessing.LabelEncoder()
+    >>> le.fit([1, 2, 2, 6])
+    LabelEncoder()
+    >>> le.classes_
+    array([1, 2, 6])
+    >>> le.transform([1, 1, 2, 6]) #doctest: +ELLIPSIS
+    array([0, 0, 1, 2]...)
+    >>> le.inverse_transform([0, 0, 1, 2])
+    array([1, 1, 2, 6])
+
+    It can also be used to transform non-numerical labels (as long as they are
+    hashable and comparable) to numerical labels.
+
+    >>> le = preprocessing.LabelEncoder()
+    >>> le.fit(["paris", "paris", "tokyo", "amsterdam"])
+    LabelEncoder()
+    >>> list(le.classes_)
+    ['amsterdam', 'paris', 'tokyo']
+    >>> le.transform(["tokyo", "tokyo", "paris"]) #doctest: +ELLIPSIS
+    array([2, 2, 1]...)
+    >>> list(le.inverse_transform([2, 2, 1]))
+    ['tokyo', 'tokyo', 'paris']
+
+    See also
+    --------
+    sklearn.preprocessing.OneHotEncoder : encode categorical integer features
+        using a one-hot aka one-of-K scheme.
+    """
+
+
+    def fit(self, y):
+        """Fit label encoder
+
+        Parameters
+        ----------
+        y : array-like of shape (n_samples,)
+            Target values.
+
+        Returns
+        -------
+        self : returns an instance of self.
+        """
+        y = column_or_1d(y, warn=True)
+        self.classes_ = np.unique(y)
+        return self
+
+
+    def fit_transform(self, y):
+        """Fit label encoder and return encoded labels
+
+        Parameters
+        ----------
+        y : array-like of shape [n_samples]
+            Target values.
+
+        Returns
+        -------
+        y : array-like of shape [n_samples]
+        """
+        y = column_or_1d(y, warn=True)
+        self.classes_, y = np.unique(y, return_inverse=True)
+        return y
+
+
+    def transform(self, y):
+        """Transform labels to normalized encoding.
+
+        Parameters
+        ----------
+        y : array-like of shape [n_samples]
+            Target values.
+
+        Returns
+        -------
+        y : array-like of shape [n_samples]
+        """
+        check_is_fitted(self, 'classes_')
+        y = column_or_1d(y, warn=True)
+
+
+        classes = np.unique(y)
+        if len(np.intersect1d(classes, self.classes_)) < len(classes):
+            diff = np.setdiff1d(classes, self.classes_)
+            raise ValueError("y contains new labels: %s" % str(diff))
+        return np.searchsorted(self.classes_, y)
+
+
+    def inverse_transform(self, y):
+        """Transform labels back to original encoding.
+
+        Parameters
+        ----------
+        y : numpy array of shape [n_samples]
+            Target values.
+
+        Returns
+        -------
+        y : numpy array of shape [n_samples]
+        """
+        check_is_fitted(self, 'classes_')
+
+
+        diff = np.setdiff1d(y, np.arange(len(self.classes_)))
+        if diff:
+            raise ValueError("y contains new labels: %s" % str(diff))
+        return self.classes_[y]
+        y = np.asarray(y)
+
+
