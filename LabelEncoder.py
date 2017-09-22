53debc2a974ef469dcebe198cede0e3835baba61
avivbd <avivbd@users.noreply.github.com>
2017-09-22 16:14:29 -0700

    Revamped LabelEncoder to include option to define values to consider as nans

--- LabelEncoder.py
+++ LabelEncoder.py
@@ -8,141 +8,84 @@ import pandas as pd
 
 
 class LabelEncoder(BaseEstimator, TransformerMixin):
-    """Encode labels with value between 0 and n_classes-1.
 
-    Read more in the :ref:`User Guide <preprocessing_targets>`.
-    Modified from sklearn by AB Aug 28 2017
-
-    Attributes
-    ----------
-    classes_ : array of shape (n_class,)
-        Holds the label for each class.
-
-    Examples
-    --------
-    >>> le = LabelEncoder()
-    >>> y_train = ["paris", "paris", "tokyo"]
-    >>> le.fit(y_train)
-    >>> le.classes_
-    array(['paris', 'tokyo'],dtype='|S8')
-
-    >>> le.transform(y_train)
-    array([0, 0, 1])
-
-    >>> y_test = ["tokyo", "tokyo", "paris", "Amsterdam"]
-    >>> le.transform(y_test)
-
-    UserWarning: y contains new labels! 
-    array([ 0,  0,  1, -1])
-
-    Note that unknown classes are mapped to n+1 where n is the size of the training dictionary.
-    The unknown classes are saved in:
-
-    >>> le.unknown_classes
-    array(['Amsterdam'], dtype='|S9')
-
-    >>> le.inverse_transform(np.array([0, 0, 1, -1, 0, 2]))
-    array(['paris', 'paris', 'tokyo', 'unknown', 'paris', 'unknown'], dtype=object)
-
-    See also
-    --------
-    sklearn.preprocessing.OneHotEncoder : encode categorical integer features
-        using a one-hot aka one-of-K scheme.
-    """
-
-    def __init__(self):
-	self.unknown_classes = []
-	self.classes_ = []
-
-    def fit(self, y):
-        """Fit label encoder
-
-        Parameters
-        ----------
-        y : array-like of shape (n_samples,)
-            Target values.
-
-        Returns
-        -------
-        self : returns an instance of self.
-        """
-        y = column_or_1d(y, warn=True)
-        self.classes_ = np.unique(y)
-        return self
-
+    def __init__(self, nan_classes=[], retain_nan=False, drop_unknown_classes=False):
+        self.unknown_classes = []
+        self.classes = []
+        self.nan_classes = nan_classes
+        self.retain_nan = retain_nan
+        self.drop_unknown_classes = drop_unknown_classes
+        
+        
     def fit_transform(self, y):
-        """Fit label encoder and return encoded labels
+        y = pd.Series(y, dtype=np.object);
+        self.classes, indcs = np.unique(y, return_inverse=True)
+        if self.retain_nan:
+            indcs = pd.Series(indcs, dtype=np.object)
+            indcs = self._ind_to_nan(indcs, y)
+            self.classes = set(self.classes) - set(self.nan_classes)
+            indcs = np.array(indcs, dtype=np.object)
+        return indcs
 
-        Parameters
-        ----------
-        y : array-like of shape [n_samples]
-            Target values.
+    def fit(self, y_train):
+        y_train = pd.Series(y_train, dtype=np.object)
+        self.classes = pd.Series(y_train.unique())
+        if self.retain_nan:
+            self.classes = self.classes[np.invert(self.classes.isin(self.nan_classes))]
+        
 
-        Returns
-        -------
-        y : array-like of shape [n_samples]
-        """
-        y = column_or_1d(y, warn=True)
-        self.classes_, y = np.unique(y, return_inverse=True)
-
-        return y
-
-    def transform(self, y, drop=False):
-        """Transform labels to normalized encoding.
-
-        Parameters
-        ----------
-        y : array-like of shape [n_samples]
-            Target values.
-
-        Returns
-        -------
-        y : array-like of shape [n_samples]
-        """
-        check_is_fitted(self, 'classes_')
-        y = column_or_1d(y, warn=True)
-
-        classes = np.unique(y)
-        diff = np.setdiff1d(classes, self.classes_)
+    def transform(self, y_test):
+        check_is_fitted(self, 'classes')
+        y_test = pd.Series(y_test, dtype=np.object)
+        test_classes = pd.Series(y_test.unique())
+        if self.retain_nan:
+            test_classes = test_classes[np.invert(test_classes.isin(self.nan_classes))]
+        invert_s = pd.Series(index=self.classes, 
+                             data=self.classes.index.values, dtype=np.object)
+        inds_s = y_test.map(invert_s)
+        
+        if self.retain_nan:
+            inds_s = self._ind_to_nan(inds_s, y_test)
+        
+        diff = set(test_classes) - set(self.classes)
         if list(diff):
+            warnings.warn("y contains new labels!")
             self.unknown_classes = diff
-            warnings.warn("y contains new labels! See LabelEncoder.unknown_classes")
-            is_unknown_cls = np.in1d(y, self.unknown_classes) 
-            inds_ar = np.searchsorted(self.classes_, y)
-            inds_ar[is_unknown_cls] = len(self.classes_)
-            if drop==True:
-                return inds_ar[np.invert(is_unknown_cls)]
-            else:
-                return inds_ar
+            is_unknown_cls = y_test.isin(self.unknown_classes)
+            inds_s[is_unknown_cls] = len(self.classes)
+            if self.drop_unknown_classes:
+                inds_s = inds_s[np.invert(is_unknown_cls)]
+                
+        return inds_s.values
+
             
-        else:
-            return np.searchsorted(self.classes_, y)
 
     def inverse_transform(self, y):
-        """Transform labels back to original encoding.
 
-        Parameters
-        ----------
-        y : numpy array of shape [n_samples]
-            Target values.
-
-        Returns
-        -------
-        y : numpy array of shape [n_samples]
-        """
         #is y a numpy array?
         assert type(y).__module__ == np.__name__
         
-        check_is_fitted(self, 'classes_')
-        out_of_range_values = np.setdiff1d(y, range(len(self.classes_)))
+        check_is_fitted(self, 'classes')
+        out_of_range_values = np.setdiff1d(y, range(len(self.classes)))
         is_out_of_range = np.in1d(y, out_of_range_values)
         has_out_of_range = np.any(is_out_of_range)
         if has_out_of_range:
             warnings.warn("y contains unknown labels")
             z = np.empty(np.size(y), dtype=np.object)
             is_in_range = np.invert(is_out_of_range)
-            z[is_in_range] = self.classes_[y[is_in_range]]
+            z[is_in_range] = self.classes[y[is_in_range]]
             z[is_out_of_range] = "unknown"
             return z
         else:
-            return self.classes_[y]
+            return self.classes[y]
+
+    def _ind_to_nan(self, indcs, y):
+        indcs[pd.isnull(y)] = np.nan
+        for nanclass in self.nan_classes:
+            indcs[y==nanclass] = nanclass
+        return indcs    
+                
+
+
+        
+        
